639e47511bb7afd2510c381bd0347fe2583dea8b
Saroar Hossain <limonhossain57@gmail.com>
2023-11-01 20:48:54 +0600

    Added: Basic setup for the plugin

---
+++ wp-contributors.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: WP Contributors
+ * Description: A basic contributors plugin for WordPress.
+ * Version: 1.0.0
+ * Author: Saroar Hossain
+ * Author URI: https://saroar.me
+ * Text Domain: wp-contributors
+ * Domain Path: /languages
+ * License: GPLv2+
+ * Requires at least: 5.5
+ * Requires PHP: 7.4
+ */
+
+declare(strict_types=1);
